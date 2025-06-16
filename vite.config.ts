dbf235183c6cfd3bef550dddfd6d97d8492bbcb5
gabrielmendes98 <gabrielmssantiago@gmail.com>
2025-06-16 17:15:42 -0300

    chore: update package.json and vite.config.ts for improved build process and CSS handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import { extname, relative, resolve } from "path";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+import { fileURLToPath } from "url";
+import { glob } from "glob";
+import type { PreRenderedAsset } from "rollup";
 
 export default defineConfig({
   plugins: [
@@ -8,19 +12,42 @@ export default defineConfig({
       tsconfigPath: "./tsconfig.json",
       include: ["src"],
     }),
+    libInjectCss(),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "UIComponents",
+      name: "ui-components",
       fileName: "ui-components",
-      formats: ["es", "umd"],
+      formats: ["es"],
     },
     rollupOptions: {
       external: [],
       output: {
-        globals: {},
+        assetFileNames: (assetInfo: PreRenderedAsset) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            const componentName = assetInfo.name.replace(".css", "");
+            return `styles/${componentName}.css`;
+          }
+          return "assets/[name][extname]";
+        },
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        inlineDynamicImports: false,
       },
+      input: Object.fromEntries(
+        glob
+          .sync("src/**/*.{ts,tsx}", {
+            ignore: ["src/**/*.d.ts", "src/**/*.test.ts", "src/**/*.test.tsx"],
+          })
+          .map((file) => [
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
     },
+    cssCodeSplit: true,
+    minify: true,
+    sourcemap: true,
   },
 });
